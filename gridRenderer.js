2a8fbdb58c96621c45b2b14d927e39245877d00f
Graham Preston <grahampreston@comcast.net>
2022-01-29 14:07:45 -0500

    Grid Renderer

---
+++ gridRenderer.js
@@ -0,0 +1,34 @@
+const EMPTY = 0;
+const WALL = 1;
+const START = 2;
+const END = 3;
+
+const drawGrid = (context, grid) => {
+	const [rowCount, colCount] = [grid.length, grid[0].length];
+
+	const size = Math.floor(Math.min(window.innerWidth / colCount, window.innerHeight / rowCount));
+	const transform = context.getTransform();
+	context.translate(-colCount * size / 2, -rowCount * size / 2);
+		
+	for (let row = 0; row < rowCount; row++) {
+		for (let col = 0; col < colCount; col++) {
+			const cell = grid[row][col];
+			if (cell === WALL) {
+				context.fillStyle = "#777";
+				context.fillRect(col * size, row * size, size, size);
+			} else if (cell === START) {
+				context.fillStyle = "#FF00FF";
+				context.fillRect(col * size, row * size, size, size);
+			} else if (cell === END) {
+				context.fillStyle = "#00FF00";
+				context.fillRect(col * size, row * size, size, size);
+			} else {
+				context.fillStyle = "#FFF";
+				context.fillRect(col * size, row * size, size, size);
+			}
+		}
+	}
+	context.setTransform(transform);
+};
+
+export default drawGrid;
--- main.js
+++ main.js
@@ -1,23 +1,5 @@
-const Canvas = () => {
-	const ratio = window.devicePixelRatio;
-	const canvas = document.createElement("canvas");
-	const ctx = canvas.getContext("2d");
-
-	const resize = () => {
-		const [width, height] = [window.innerWidth, window.innerHeight];
-		canvas.width = width * ratio;
-		canvas.height = height * ratio;
-		canvas.style.width = `${width}px`;
-		canvas.style.height = `${height}px`;
-		ctx.scale(ratio, ratio);
-		ctx.translate(width / 2, height / 2);
-	};
-	
-	resize();
-	window.addEventListener("resize", resize);
-
-	return canvas;
-};
+import gridDraw from "./gridRenderer.js";
+import Canvas from "./canvas.js";
 
 let canvas;
 let context;
@@ -25,29 +7,30 @@ let context;
 const setup = () => {
 	canvas = Canvas();
 	context = canvas.getContext("2d");
-	context.fillStyle = "black";
+	context.fillStyle = "#000";
 	context.fillRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
 
 	document.body.appendChild(canvas);
 };
 
-const grid = (width, height) => {
-	const size = Math.floor(Math.min(window.innerWidth / width, window.innerHeight / height));
-	context.translate(-width * size / 2, -height * size / 2);
+const update = () => {
+	// Reset frame.
+	context.fillStyle = "#000";
+	context.fillRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
 
-	context.strokeStyle = "#FF00FF";
-	context.lineWidth = size / 10;
-	context.fillStyle = "#333";
-	
-	for (let x = 0; x < width; x++) {
-		for (let y = 0; y < height; y++) {
-			context.strokeRect(x * size, y * size, size, size);
-			context.fillRect(x * size, y * size, size, size);
-		}
-	}
+	const grid = [
+		[2, 1, 1, 1, 0], 
+		[0, 0, 1, 1, 0], 
+		[1, 0, 0, 0, 1],
+		[1, 0, 0, 0, 3]
+	];
+	gridDraw(context, grid);
+
+	// Draw next frame.
+	requestAnimationFrame(() => update());
 };
 
 (() => {
 	setup();
-	grid(15, 10);
+	update();
 })();
