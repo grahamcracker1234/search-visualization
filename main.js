a3285ca90c5dd96a5936fda2d511b944b81ca595
Graham Preston <grahampreston@comcast.net>
2022-01-29 12:05:44 -0500

    grib

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const Canvas = () => {
 		canvas.style.width = `${width}px`;
 		canvas.style.height = `${height}px`;
 		ctx.scale(ratio, ratio);
-		ctx.translate(width / 2, height / 2);
+		// ctx.translate(width / 2, height / 2);
 	};
 	
 	resize();
@@ -19,15 +19,32 @@ const Canvas = () => {
 	return canvas;
 };
 
+let canvas;
+let context;
+
 const setup = () => {
-	const canvas = Canvas();
-	const context = canvas.getContext("2d");
+	canvas = Canvas();
+	context = canvas.getContext("2d");
 	context.fillStyle = "grey";
 	context.fillRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
 
 	document.body.appendChild(canvas);
 };
 
+const grid = () => {
+	const size = 100;
+	const width = 10;
+	const height = 10;
+	context.strokeStyle = "red";
+	// context.translate(width * size / 2, height * size / 2);
+	for (let x = 0; x < width; x++) {
+		for (let y = 0; y < height; y++) {
+			context.strokeRect(x * size, y * size, size, size);
+		}
+	}
+};
+
 (() => {
 	setup();
+	grid();
 })();
